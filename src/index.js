8930eec1d61ad43e733720d7af35b6f0c3baaad2
root <root@localhost.localdomain>
2018-07-13 20:57:26 +0530

    module 2 work in progress

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.css';
+
+import numeral from 'numeral';
+
+const  courseVal = numeral(1000).format('$0,0.00');
+
+debugger; //eslint-disable-line no-debugger
+console.log(`I would pay ${courseVal} for this awesome course !`);//eslint-disable-line no-console
